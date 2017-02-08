61a411556f9e886bc8c0ab9da08cca81db5de62a
Jasdero <houssemaine.j@gmail.com>
2017-02-08 10:37:07 +0100

    index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Password</title>
+</head>
+<body>
+<h2>Access to NASA datas</h2>
+<form action="pass.php">
+    <label for="pass">Enter yout password</label>
+    <input type="text" name="pass" id="pass">
+    <input type="submit" value="Send">
+</form>
+</body>
+</html>
